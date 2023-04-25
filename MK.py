5c77345d2148d0c51d04bb98cd29e5e253b5f059
wkim31 <won_kyung_kim@brown.edu>
2023-04-24 22:58:18 -0400

    Background set up
    Setting up just the background within the game loop. Have to adjust size.

--- MK.py
+++ MK.py
@@ -1,37 +1,24 @@
 #imports:
 import pygame
-from pygame.locals import (
-    RLEACCEL,
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+from pygame.locals import *
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([1000, 1000])
+WIDTH = 1000
+HEIGHT = 1000
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+bg_image = pygame.image.load('fin_bg-mario-circuit-3.png')
+bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
 
 # Run until the user asks to quit
 running = True
 while running:
     # Did the user click the window close button?
+    screen.blit(bg_image, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # Fill the background with white
-    screen.fill((255, 255, 255))
-    #pygame.image.load(
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-    # Flip the display
-    pygame.display.flip()
+    pygame.display.update()
 
 # Done! Time to quit.
-pygame.quit()
+pygame.quit() 
