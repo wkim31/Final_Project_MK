81f5ec99a8b7cfa72a2cf069a7b90610a1ce1273
wkim31 <won_kyung_kim@brown.edu>
2023-04-24 21:50:31 -0400

    initial game loop

---
+++ MK.py
@@ -0,0 +1,37 @@
+#imports:
+import pygame
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([1000, 1000])
+
+# Run until the user asks to quit
+running = True
+while running:
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+    #pygame.image.load(
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
